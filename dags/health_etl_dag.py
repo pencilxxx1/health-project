768d7554c33c269b51ca604b974046572004fc5b
Akeem <agboolakeem00@gmail.com>
2025-05-15 23:09:19 +0200

    first pipeline

--- dags/health_etl_dag.py
+++ dags/health_etl_dag.py
@@ -1,1 +1,70 @@
 # Dag will run here
+from datetime import datetime, timedelta
+from airflow import models
+from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow .providers.google.clod.operators.dataflow import DataflowCreatePipelineOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+
+
+default_args = {
+    'owner': '10alytics',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+
+}
+
+# run the sql script to create the tables
+def get_sql_from_gcs(**context):
+    gcs_hook = GCSHook(gcp_conn_id='google_cloud_default')
+    bucket_name = 'bucket-ak-health-project'
+    file_name = 'sql/create_star_schema.sql'
+    file_content = gcs_hook.download_as_byte_array(bucket_name=bucket_name, object_name=file_name).decode('utf-8')
+    return file_content
+
+
+
+with models.DAG(
+    dag_id='health_etl_dag',
+    default_args=default_args,
+    description='Daily Health Tech Dag That Fetches Data from a PostgresDB and Loads it into BigQuery',
+    schedule_interval=timedelta(days=1),
+) as dag:
+    start = EmptyOperator(task_id='start')
+
+    # Task to fetch SQL from GCS
+    fetch_sql = PythonOperator(
+        task_id='fetch_sql',
+        python_callable=get_sql_from_gcs,
+        provide_context=True,
+    )
+
+    # Task to create star schema in BigQuery
+    create_star_schema = BigQueryInsertJobOperator(
+        task_id='create_star_schema',
+        configuration={
+            "query": {
+                "query": "{{ task_instance.xcom_pull(task_ids='fetch_sql') }}",
+                "useLegacySql": False,
+            }
+        },
+        location='us-east1',
+        project_id='healthcare-project-459415',
+    )
+
+    end = EmptyOperator(task_id='end')
+
+    start >> fetch_sql >> create_star_schema >> end
+    
+
+
+
+# import necessary libraries
+
+# initialize the DAG
+
+
+
